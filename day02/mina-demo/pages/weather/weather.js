97db4ae81d40afb6582ceafda1cc4346e6414175
thu <mingfei.net@gmail.com>
2017-12-08 17:02:34 +0800

    Update

--- day02/mina-demo/pages/weather/weather.js
+++ day02/mina-demo/pages/weather/weather.js
@@ -1,1 +1,11 @@
-Page({})
+Page({
+  data: {
+    city: null
+  },
+  getCity: function(event){
+    this.setData({city:event.detail});
+  },
+  getWeather: function(){
+    console.log(this.data.city);
+  }
+})
