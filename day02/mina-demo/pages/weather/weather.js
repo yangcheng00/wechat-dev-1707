72e54f3d24832b0393cfc21e6641e5b150219b5b
thu <mingfei.net@gmail.com>
2017-12-08 18:04:39 +0800

    Update

--- day02/mina-demo/pages/weather/weather.js
+++ day02/mina-demo/pages/weather/weather.js
@@ -1,16 +1,18 @@
 Page({
   data: {
-    city: null
+    city: null,
+    data: null
   },
   getCity: function(event){
     this.setData({city:event.detail.value});
   },
   getWeather: function(){
     let urlString = 'https://api.jisuapi.com/weather/query?appkey=9ef825ad675bffb3&city=' + this.data.city;
+    let page = this; // ***
     wx.request({
       url: urlString,
       success: function(res) {
-        console.log(res);
+        page.setData({data:res.data}); 
       }
     })
   }
