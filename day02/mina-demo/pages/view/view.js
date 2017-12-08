85301b5490f4869ebe80399755b3d1c5ba279d33
thu <mingfei.net@gmail.com>
2017-12-08 16:01:43 +0800

    Update

--- day02/mina-demo/pages/view/view.js
+++ day02/mina-demo/pages/view/view.js
@@ -1,5 +1,11 @@
 Page({
   data: {
     message: 'new text...'
+  },
+  changeMessage: function(event){
+    console.log(event.type);
+    this.setData({
+      message: 'text...'
+    });
   }
 });
